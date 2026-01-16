bb68152bd9a9629320bc9cb5940a672de9e66b99
saiegh@gmail.com <saiegh@gmail.com>
2026-01-16 00:23:00 -0300

    probably neighbours issue fixed

--- demos/tree.js
+++ demos/tree.js
@@ -19,10 +19,6 @@ export class Tree extends GameObject {
   static components = [Collider, SpriteRenderer, ShadowCaster];
 
   setup() {
-    // Override Boid's physics properties for prey behavior
-    // this.rigidBody.maxVel = 0;
-    // this.rigidBody.maxAcc = 0;
-    // this.rigidBody.static = 1; // Static body - nothing can move it
     this.setSprite("tree" + (Math.random() > 0.5 ? 1 : 2));
     this.scale = Math.random() * 0.5 + 1;
     this.setScale(Math.random() > 0.5 ? this.scale : -this.scale, this.scale);
@@ -40,16 +36,5 @@ export class Tree extends GameObject {
     this.setup();
     //this should not be needed, i guess:
     //TODO: make onSpawned() also execute this.setup() by default
-  }
-
-  onDespawned() {
-    // Could save stats, play death effects, etc.
-  }
-
-  tick(dtRatio) {
-    // Solo loggear la primera TallLight para no spamear
-    // console.log(
-    //   `House[${this.index}] neighbors: ${this.neighborCount}, visualRange: ${this.collider.visualRange}, active: ${this.collider.active}`
-    // );
   }
 }
