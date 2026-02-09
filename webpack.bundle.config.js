99417c33ce5a9adcf92d97999988888c46b67770
saiegh@gmail.com <saiegh@gmail.com>
2026-02-09 12:21:11 -0300

    make bundle dist

---
+++ webpack.bundle.config.js
@@ -0,0 +1,103 @@
+/**
+ * Webpack config for SINGLE FILE bundle
+ * Workers are loaded as raw strings and embedded in WEED.WorkerSources
+ */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import TerserPlugin from 'terser-webpack-plugin';
+import WebpackObfuscator from 'webpack-obfuscator';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const shouldObfuscate = process.env.OBFUSCATE === 'true';
+
+const obfuscatorOptions = {
+    rotateStringArray: true,
+    stringArray: true,
+    stringArrayThreshold: 0.75,
+    deadCodeInjection: false,
+    debugProtection: false,
+    disableConsoleOutput: false,
+    identifierNamesGenerator: 'hexadecimal',
+    log: false,
+    numbersToExpressions: true,
+    renameGlobals: false,
+    selfDefending: false,
+    simplify: true,
+    splitStrings: true,
+    splitStringsChunkLength: 10,
+    transformObjectKeys: true,
+    unicodeEscapeSequence: false
+};
+
+export default {
+    mode: 'production',
+    devtool: false,
+    entry: './src/index.bundle.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'weed.bundle.min.js',
+        library: {
+            name: 'WEED',
+            type: 'umd',
+            export: 'default'
+        },
+        globalObject: 'typeof self !== "undefined" ? self : this',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, /\.worker\.bundled\.js$/],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: { browsers: ['> 1%', 'last 2 versions', 'not dead'] },
+                                modules: false
+                            }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // Load pre-compiled worker files as raw strings
+            {
+                test: /\.worker\.bundled\.js$/,
+                type: 'asset/source'
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: false,
+                        drop_debugger: true,
+                        pure_funcs: ['console.debug']
+                    },
+                    mangle: {
+                        reserved: ['WEED', 'GameEngine', 'Scene', 'GameObject', 'Component']
+                    },
+                    format: {
+                        comments: false
+                    }
+                },
+                extractComments: false
+            })
+        ]
+    },
+    plugins: [
+        ...(shouldObfuscate ? [new WebpackObfuscator(obfuscatorOptions, [])] : [])
+    ],
+    resolve: {
+        extensions: ['.js']
+    }
+};
